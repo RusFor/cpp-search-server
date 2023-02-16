0649e552d18f5fb232cc64b322566081be432148
Dmitrii Sychev <cpt.rouchxant@gmail.com>
2023-02-16 14:39:13 +0300

    Изменил. Сделал мултисет, потому что есть вероятность, что документ будет иметь другой смысл из - за повтораяющегося слова.

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -1,35 +1,27 @@
 #include "remove_duplicates.h"
 
 void RemoveDuplicates(SearchServer& search_server) {
-	std::vector<std::pair<std::vector<std::string>, int>> text_id;
+	std::map<std::multiset<std::string>, int> m_words;
 	auto data = search_server.GetDocuments();
-	std::vector<std::string> str{};
+	std::multiset<std::string> str{};
 	for (const auto& [id, words] : data) {
 		for (const auto& [word, freq] : words) {
-			str.push_back(word);
+			str.insert(word);
 		}
-		sort(str.begin(), str.end());
-		text_id.push_back({ str, id });
-		str.clear();
-	}
-
-	for (int i = 0; i < text_id.size() - 1; ++i) {
-		for (int j = i + 1; j < text_id.size(); ++j) {
-			if (text_id[i].first == text_id[j].first) {
-				int del;
-				if (text_id[i].second < text_id[j].second) {
-					del = text_id[j].second;
-					text_id.erase(text_id.begin() + j);
-					--j;
-				}
-				else {
-					del = text_id[i].second;
-					text_id.erase(text_id.begin() + i);
-					--i;
-				}
-				std::cout << "Found duplicate document id "s << del << std::endl;
-				search_server.RemoveDocument(del);
+		if (m_words.count(str)) {
+			int del;
+			if (m_words.at(str) > id) {
+				del = m_words.at(str);
+				m_words.at(str) = id;
 			}
-		}
+			else {
+				del = id;
+			}
+			std::cout << "Found duplicate document id "s << del << std::endl;
+			search_server.RemoveDocument(del);
+		} else {
+		m_words[str] = id;
+	}
+		str.clear();
 	}
 }
